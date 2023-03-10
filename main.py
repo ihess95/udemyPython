22bb55a802ccd32b0b0b2e97b2ce4f46861ae139
Ian Hess <ian.h.hess@gmail.com>
2023-03-10 12:01:23 -0500

    add display and _case

--- main.py
+++ main.py
@@ -2,14 +2,18 @@ todos = []
 
 while True:
     user_action = input("Type add or show or q to quit: ")
+    user_action = user_action.strip()
 
     match user_action:
         case 'add':
             todo = input("Enter a todo: ")
             todos.append(todo)
-        case 'show':
-            print(todos)
+        case 'show' | 'display':
+            for item in todos:
+                print(item)
         case 'q':
             break
+        case _:
+            print("Hey, you entered an invalid input.")
 
 print("exited")
