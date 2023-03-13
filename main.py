fc188244990dec5b977b9eed67f404fa8b5339b6
Ian Hess <ian.h.hess@gmail.com>
2023-03-13 10:01:39 -0400

    write match case

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 todos = []
 
 while True:
-    user_action = input("Type add or show or q to quit: ")
+    user_action = input("Type add, show, edit or q to quit: ")
     user_action = user_action.strip()
 
     match user_action:
@@ -13,6 +13,14 @@ while True:
                 print(item)
         case 'q':
             break
+        case 'edit':
+            try:
+                to_edit = input('Which todo would you like to edit? ')
+                edit = input(f'What would you like to change {todos[int(to_edit)-1]} to? ')
+                todos[int(to_edit)-1] = edit
+            except:
+                print(f"That is not a valid task. Please choose a number between one and {len(todo)-1}")
+            
         case _:
             print("Hey, you entered an invalid input.")
 
